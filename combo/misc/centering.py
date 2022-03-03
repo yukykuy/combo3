2baef8f67b1894a6a4d46d04e5c2e9b9eefd5f4e
yukykuy <95830407+yukykuy@users.noreply.github.com>
2022-03-03 10:47:55 +0900

    Update centering.py
    fix the condition of centering (float64 keeps only 15 decimal digits)

--- combo/misc/centering.py
+++ combo/misc/centering.py
@@ -3,5 +3,5 @@ import numpy as np
 
 def centering(X):
     stdX = np.std(X, 0)
-    index = np.where(stdX != 0)
+    index = np.where(stdX > 10**(-15))
     return (X[:, index[0]] - np.mean(X[:, index[0]], 0)) / stdX[index[0]]
